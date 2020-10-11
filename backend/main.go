4c63037aef0040168ceba733ebb5d30dedf7624c
radiantwanderer <radiantgamewanderer@gmail.com>
2020-10-11 12:05:57 -0500

    implements example routes that require user authentication

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,43 @@
 package main
 
 import (
-    //"encoding/json"
+    "encoding/json"
     "fmt"
-    "github.com/gorilla/mux"
     "net/http"
+    "strings"
+	"errors"
+	//"log"
+	//"os"
 
+    "github.com/codegangsta/negroni"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+    "github.com/gorilla/mux"
+    "github.com/rs/cors"
     "github.com/radiantwanderer/RadChat/pkg/websocket"
 )
+
+type Response struct {
+    Message string `json:"message"`
+}
+
+type Jwks struct {
+    Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+    Kty string `json:"kty"`
+    Kid string `json:"kid"`
+    Use string `json:"use"`
+    N string `json:"n"`
+    E string `json:"e"`
+    X5c []string `json:"x5c"`
+}
+
+type CustomClaims struct {
+    Scope string `json:"scope"`
+    jwt.StandardClaims
+}
 
 func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
     fmt.Println("WebSocket Endpoint Hit")
@@ -40,11 +70,142 @@ func main() {
     fmt.Println("RadChat v0.02")
     setupRoutes()
 
+    jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options {
+        ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+            //Verify 'aud' claim
+            aud := "https://radchatauth/api"
+            checkAud := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+            if !checkAud {
+                return token, errors.New("Invalid audience.")
+            }
+            // Verify 'iss' claim
+            iss := "https://dev--48759fe.us.auth0.com/"
+            checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+            if !checkIss {
+                return token, errors.New("Invalid issuer.")
+            }
+
+            cert, err := getPemCert(token)
+            if err != nil {
+                panic(err.Error())
+            }
+
+            result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+            return result, nil
+        },
+        SigningMethod: jwt.SigningMethodRS256,
+    })
+
+    c := cors.New(cors.Options{
+        AllowedOrigins: []string{"http://localhost:8080"},
+        AllowCredentials: true,
+        AllowedHeaders: []string{"Authorization"},
+    })
+
     r := mux.NewRouter()
 
-    r.Handle("/", http.FileServer(http.Dir("./views/")))
+    //Always accessible route
+    r.Handle("/api/public", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            message := "Hello from a public endpoint! You don't need to be authenticated to see this."
+            responseJSON(message, w, http.StatusOK)
+        }))
 
-    r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+    //Route only accessible if user has a valid Access Token
+    r.Handle("/api/private", negroni.New(
+            negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+            negroni.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+                message := "Hello from a private endpoint! You need to be authenticated to see this"
+                responseJSON(message, w, http.StatusOK)
+    }))))
 
-    http.ListenAndServe(":8080", nil)
+    r.Handle("/api/private-scoped", negroni.New(
+        negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+        negroni.Wrap(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+            authHeaderParts := strings.Split(r.Header.Get("Authorization"), " ")
+            token := authHeaderParts[1]
+
+            hasScope := checkScope("read:messages", token)
+
+            if !hasScope {
+                message := "Insufficient scope."
+                responseJSON(message, w, http.StatusForbidden)
+                return
+            }
+            message := "Hello from a private endpoint! You need to be authenticated to see this."
+            responseJSON(message, w, http.StatusOK)
+    }))))
+
+    handler := c.Handler(r)
+    http.Handle("/", r)
+
+    http.ListenAndServe(":8080", handler)
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+    cert := ""
+    resp, err := http.Get("https://dev--48759fe.us.auth0.com/.well-known/jwks.json")
+
+    if err != nil {
+        return cert, err
+    }
+    defer resp.Body.Close()
+
+    var jwks = Jwks{}
+    err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+    if err != nil {
+        return cert, err
+    }
+
+    for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+    if cert == "" {
+        err := errors.New("Unable to find appropriate key.")
+        return cert, err
+    }
+
+    return cert, nil
+}
+
+func responseJSON(message string, w http.ResponseWriter, statusCode int) {
+    response := Response{message}
+
+    jsonResponse, err := json.Marshal(response)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(statusCode)
+    w.Write(jsonResponse)
+}
+
+func checkScope(scope string, tokenString string) bool {
+    token, _ := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func (token *jwt.Token) (interface{}, error) {
+		cert, err := getPemCert(token)
+		if err != nil {
+			return nil, err
+		}
+		result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+		return result, nil
+	})
+
+    claims, ok := token.Claims.(*CustomClaims)
+
+    hasScope := false
+    if ok && token.Valid {
+        result := strings.Split(claims.Scope, " ")
+        for i := range result {
+            if result[i] == scope {
+                hasScope = true
+            }
+        }
+    }
+
+    return hasScope
 }
